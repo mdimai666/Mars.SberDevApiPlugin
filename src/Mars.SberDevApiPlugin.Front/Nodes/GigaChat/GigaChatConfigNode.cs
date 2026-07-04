039a13be258a381fa707472ea44b8c3f4e016e0f
mdimai666 <mdimai666@mail.ru>
2026-07-05 04:50:53 +0900

    upd 0.7.7-alpha.19

--- src/Mars.SberDevApiPlugin.Front/Nodes/GigaChat/GigaChatConfigNode.cs
+++ src/Mars.SberDevApiPlugin.Front/Nodes/GigaChat/GigaChatConfigNode.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using Mars.Core.Attributes;
-using Mars.Nodes.Core.Nodes;
+using Mars.Nodes.Core.Nodes.Common;
 
 namespace Mars.SberDevApiPlugin.Front.Nodes.GigaChat;
 
